9e68e65f7a185f363fc0dc5c152b6b00b4055b80
karthik rao <karthik.rao543@gmail.com>
2019-12-24 20:09:46 -0500

    saving progress

---
+++ src/main/java/com/pantheon/core/utils/MathUtils.java
@@ -0,0 +1,22 @@
+package com.pantheon.core.utils;
+
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
+public class MathUtils {
+
+    private static final Vector3f xAxis = new Vector3f(1,0,0);
+    private static final Vector3f yAxis = new Vector3f(0,1,0);
+    private static final Vector3f zAxis = new Vector3f(0,0,1);
+
+    public static Matrix4f createTransformationMatrix(Vector3f translation, float rx, float ry, float rz, float scale) {
+        Matrix4f mat = new Matrix4f();
+        mat.identity();
+        mat.translate(translation, mat);
+        mat.rotate((float) Math.toRadians(rx), xAxis, mat);
+        mat.rotate((float) Math.toRadians(ry), yAxis, mat);
+        mat.rotate((float) Math.toRadians(rz), zAxis, mat);
+        mat.scale(new Vector3f(scale, scale, scale), mat);
+        return mat;
+    }
+}
--- src/main/resources/shaders/vertex.glsl
+++ src/main/resources/shaders/vertex.glsl
@@ -5,8 +5,11 @@ in vec2 textCoords;
 
 out vec2 pass_textCoords;
 
+//uniform mat4 transformationMatrix;
+
 void main(void)
 {
+//    gl_Position = transformationMatrix * vec4(vertex_position, 1.0);
     gl_Position = vec4(vertex_position, 1.0);
     pass_textCoords = textCoords;
 }
